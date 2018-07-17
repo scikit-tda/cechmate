7d9672890deb7e3376de2e792e4a4c62b1fde70b
Chris Tralie <chris.tralie@gmail.com>
2018-07-16 22:59:47 -0400

    Fixing up Python wrapper by sorting simplex indices so phat doesn't hang, and getting alpha filtrations to work

--- PhatCech.py
+++ PhatCech.py
@@ -1,6 +1,10 @@
 import numpy as np
+import numpy.linalg as linalg
 import matplotlib.pyplot as plt
 import itertools
+import phat
+import warnings
+import time
 
 def plotDGM(dgm, color = 'b', sz = 20, label = 'dgm', \
         axcolor = np.array([0.0, 0.0, 0.0]), marker = None):
@@ -76,9 +80,9 @@ class Simplex(object):
         return "%s %s"%(self.idxs, self.dist)
 
 
-def getCechDGMs(simplices, returnInfs = False, useWrapper = True, Verbose = True):
+def get_phat_dgms(simplices, returnInfs = False, Verbose = True):
     """
-    Do a Cech filtration
+    Do a custom filtration wrapping around phat
     :param simplices: A list of lists of simplices and their distances\
         the kth element is itself a list of tuples ([idx1, ..., idxk], dist)\
         where [idx1, ..., idxk] is a list of vertices involved in the simplex\
@@ -90,10 +94,13 @@ def getCechDGMs(simplices, returnInfs = False, useWrapper = True, Verbose = True
         the persistence diagram for Hk 
     """
     idxs2order = {}
-    #Step 1: Go through simplices in ascending order of distance
+    ## Step 1: Go through simplices in ascending order of distance
     idx = 0
     columns = []
     ordsimplices = sorted([Simplex(s[0], s[1]) for s in simplices])
+    if Verbose:
+        print("Constructing boundary matrix...")
+        tic = time.time()
     for simplex in ordsimplices:
         (idxs, dist) = (simplex.idxs, simplex.dist)
         k = len(idxs)
@@ -113,34 +120,21 @@ def getCechDGMs(simplices, returnInfs = False, useWrapper = True, Verbose = True
                         %(idxs, fidxs))
                     return None
                 collist.append(idxs2order[fidxs])
+            collist = sorted(collist)
             columns.append((k-1, collist))
         idx += 1
-    #Step 2: Setup boundary matrix and reduce
+    ## Step 2: Setup boundary matrix and reduce
     if Verbose:
+        print("Finished constructing boundary matrix (Elapsed Time %.3g)"%(time.time()-tic))
         print("Computing persistence pairs...")
-    if useWrapper:
-        import subprocess
-        fout = open("boundary.dat", "w")
-        for (k, collist) in columns:
-            fout.write("%i "%k)
-            fout.write(("%i "*len(collist))%tuple(collist))
-            fout.write("\n")
-        fout.close()
-        subprocess.call(["./phat", "--ascii", "boundary.dat", "pairs.dat"])
-        fin = open("pairs.dat")
-        pairs = []
-        for line in fin.readlines()[1::]:
-            pairs.append([int(k.strip()) for k in line.split()])
-        fin.close()
-    else:
-        import phat
-        boundary_matrix = phat.boundary_matrix(columns = columns)
-        pairs = boundary_matrix.compute_persistence_pairs()
-        pairs.sort()
+        tic = time.time()
+    boundary_matrix = phat.boundary_matrix(columns = columns, representation = phat.representations.sparse_pivot_column)
+    pairs = boundary_matrix.compute_persistence_pairs()
+    pairs.sort()
     if Verbose:
-        print("Finished computing persistence pairs")
+        print("Finished computing persistence pairs (Elapsed Time %.3g)"%(time.time()-tic))
 
-    #Step 3: Setup persistence diagrams by reading off distances
+    ## Step 3: Setup persistence diagrams by reading off distances
     Is = {} #Persistence diagrams
     posneg = np.zeros(len(simplices))
     for [bi, di] in pairs:
@@ -161,7 +155,7 @@ def getCechDGMs(simplices, returnInfs = False, useWrapper = True, Verbose = True
             continue
         Is[p].append([bd, dd])
     
-    #Step 4: Add all unpaired simplices as infinite points
+    ## Step 4: Add all unpaired simplices as infinite points
     if returnInfs:
         for i in range(len(posneg)):
             if posneg[i] == 0:
@@ -186,7 +180,7 @@ def getSSM(X):
     D = np.sqrt(D)
     return D
 
-def doRipsFiltrationEuclidean(X, p):
+def rips_filtration(X, p):
     """
     Do the rips filtration of a Euclidean point set
     :param X: An Nxd array of N Euclidean vectors in d dimensions
@@ -209,17 +203,132 @@ def doRipsFiltrationEuclidean(X, p):
                 for j in range(i+1, len(idxs)):
                     d = max(d, D[idxs[i], idxs[j]])
             simplices.append((idxs, d))
-    return getCechDGMs(simplices)    
+    return get_phat_dgms(simplices)    
 
-def doAlphaComplexFiltration(X, p):
+def get_circumcenter(X):
     """
-    Do the Cech filtration of a Euclidean point set (requires scipy)
+    Compute the circumcenter and circumradius of a simplex
+    Parameters
+    ----------
+    X : ndarray (N, d)
+        Coordinates of points on an N-simplex in d dimensions
+    
+    Returns
+    -------
+    (circumcenter, circumradius)
+        A tuple of the circumcenter and squared circumradius.  
+        (SC1) If there are fewer points than the ambient dimension plus one,
+          then return the circumcenter corresponding to the smallest
+          possible squared circumradius
+        (SC2) If the points are not in general position, 
+          it returns (np.inf, np.inf)
+        (SC3) If there are more points than the ambient dimension plus one
+          it returns (np.nan, np.nan)
+    """
+    if X.shape[0] == 2:
+        # Special case of an edge, which is very simple
+        dX = X[1, :] - X[0, :]
+        rSqr = 0.25*np.sum(dX**2)
+        x = X[0, :] + 0.5*dX
+        return (x, rSqr)
+    if X.shape[0] > X.shape[1] + 1: # SC3 (too many points)
+        warnings.warn("Trying to compute circumsphere for " +\
+                      "%i points in %i dimensions"%(X.shape[0], X.shape[1]))
+        return (np.nan, np.nan)
+    # Transform arrays for PCA for SC1 (points in higher ambient dimension)
+    muV = np.array([])
+    V = np.array([]) 
+    if X.shape[0] < X.shape[1]+1:
+        # SC1: Do PCA down to NPoints-1
+        muV = np.mean(X, 0)
+        XCenter = X - muV
+        _, V = linalg.eigh((XCenter.T).dot(XCenter))
+        V = V[:, (X.shape[1]-X.shape[0]+1)::] #Put dimension NPoints-1
+        X = XCenter.dot(V)
+    muX = np.mean(X, 0)
+    D = np.ones((X.shape[0], X.shape[0]+1))
+    # Subtract off centroid for numerical stability
+    D[:, 1:-1] = X - muX
+    D[:, 0] = np.sum(D[:, 1:-1]**2, 1)
+    minor = lambda A, j: \
+            A[:, np.concatenate((np.arange(j), np.arange(j+1, A.shape[1])))]
+    dxs = np.array([linalg.det(minor(D, i)) for i in range(1, D.shape[1]-1)])
+    alpha = linalg.det(minor(D, 0))
+    if np.abs(alpha) > 0:
+        signs = (-1)**np.arange(len(dxs))
+        x = dxs*signs/(2*alpha) + muX #Add back centroid
+        gamma = ((-1)**len(dxs))*linalg.det(minor(D, D.shape[1]-1))
+        rSqr = (np.sum(dxs**2) + 4*alpha*gamma)/(4*alpha*alpha)
+        if V.size > 0:
+            # Transform back to ambient if SC1
+            x = x.dot(V.T) + muV
+        return (x, rSqr)
+    return (np.inf, np.inf) #SC2 (Points not in general position)
+
+def alpha_filtration(X, Verbose = True):
+    """
+    Do the Alpha filtration of a Euclidean point set (requires scipy)
     :param X: An Nxd array of N Euclidean vectors in d dimensions
     """
     from scipy.spatial import Delaunay
-    pass
 
-def doRipsFiltrationDMGUDHI(D, p, coeff = 2, doPlot = False):
+    if X.shape[0] < X.shape[1]:
+        warnings.warn(
+            "The input point cloud has more columns than rows; " +
+            "did you mean to transpose?")
+    maxdim = X.shape[1]-1
+
+    ## Step 1: Figure out the filtration
+    if Verbose:
+        print("Doing Delaunay triangulation...")
+        tic = time.time()
+    delaunay_faces = Delaunay(X).simplices
+    if Verbose:
+        print("Finished Delaunay triangulation (Elapsed Time %.3g)"%(time.time()-tic))
+        print("Building alpha filtration...")
+        tic = time.time()
+        
+    filtration = {}
+    simplices_bydim = {}
+    for dim in range(maxdim+2, 1, -1):
+        simplices_bydim[dim] = []
+        for s in range(delaunay_faces.shape[0]):
+            simplex = delaunay_faces[s, :]
+            for sigma in itertools.combinations(simplex, dim):
+                sigma = tuple(sorted(sigma))
+                simplices_bydim[dim].append(sigma)
+                if not sigma in filtration:
+                    filtration[sigma] = get_circumcenter(X[sigma, :])[1]
+                for i in range(dim):
+                    # Propagate alpha filtration value
+                    tau = sigma[0:i] + sigma[i+1::]
+                    if tau in filtration:
+                        filtration[tau] = min(filtration[tau], filtration[sigma])
+                    elif len(tau) > 1:
+                        # If Tau is not empty
+                        xtau, rtauSqr = get_circumcenter(X[tau, :])
+                        if np.sum((X[sigma[i], :]-xtau)**2) < rtauSqr:
+                            filtration[tau] = filtration[sigma]
+    for f in filtration:
+        filtration[f] = np.sqrt(filtration[f])
+
+    ## Step 2: Take care of numerical artifacts that may result
+    ## in simplices with greater filtration values than their co-faces
+    for dim in range(maxdim+2, 2, -1):
+        for sigma in simplices_bydim[dim]:
+            for i in range(dim):
+                tau = sigma[0:i] + sigma[i+1::]
+                if filtration[tau] > filtration[sigma]:
+                    filtration[tau] = filtration[sigma]
+    if Verbose:
+        print("Finished building alpha filtration (Elapsed Time %.3g)"%(time.time()-tic))
+                    
+    simplices = [([i], 0) for i in range(X.shape[0])]
+    for tau in filtration:
+        simplices.append((tau, filtration[tau]))
+    return get_phat_dgms(simplices, Verbose=Verbose)
+
+def rips__filtration_gudhi(D, p, coeff = 2, doPlot = False):
     """
     Do the rips filtration, wrapping around the GUDHI library (for comparison)
     :param X: An Nxk matrix of points
@@ -244,6 +353,9 @@ def doRipsFiltrationDMGUDHI(D, p, coeff = 2, doPlot = False):
         Is[i] = np.array(Is[i])
     return Is
 
+def alpha_filtration_gudhi(X):
+    pass
+
 def testRips(compareToGUDHI = False):
     """
     A test with a noisy circle, comparing H1 to GUDHI
@@ -254,7 +366,7 @@ def testRips(compareToGUDHI = False):
     X[:, 1] = np.sin(t)
     np.random.seed(10)
     X += 0.2*np.random.randn(len(t), 2)
-    Is = doRipsFiltrationEuclidean(X, 1)
+    Is = rips_filtration(X, 1)
     plt.subplot(131)
     plt.scatter(X[:, 0], X[:, 1], 20)
     plt.axis('equal')
@@ -265,11 +377,61 @@ def testRips(compareToGUDHI = False):
     plotDGM(Is[1])
     if compareToGUDHI:
         D = getSSM(X)
-        Is2 = doRipsFiltrationDMGUDHI(D, 1)
+        Is2 = rips__filtration_gudhi(D, 1)
         plotDGM(Is2[1], color = 'r', marker = 'x')
     plt.title("H1")
     plt.show()
 
+def testAlpha():
+    from ripser import plot_dgms
+    # Make a 3-sphere in 4 dimensions
+    X = np.random.randn(200, 4)
+    X = X/np.sqrt(np.sum(X**2, 1)[:, None])
+    tic = time.time()
+    dgms = alpha_filtration(X)
+    phattime = time.time() - tic
+    print("Elapsed Time: %.3g"%phattime)
+    plot_dgms([dgms[i] for i in range(len(dgms))])
+    plt.show()
+
+def convertGUDHIPD(pers, dim):
+    Is = []
+    for i in range(dim):
+        Is.append([])
+    for i in range(len(pers)):
+        (dim, (b, d)) = pers[i]
+        Is[dim].append([b, d])
+    #Put onto diameter scale so it matches rips more closely
+    return [np.sqrt(np.array(I)) for I in Is]
+
+def compareAlpha():
+    import gudhi
+    np.random.seed(2)
+    # Make a 4-sphere in 5 dimensions
+    X = np.random.randn(100, 5)
+    
+    tic = time.time()
+    Is1 = alpha_filtration(X)
+    phattime = time.time() - tic
+    print("Phat Time: %.3g"%phattime)
+    
+    tic = time.time()
+    alpha_complex = gudhi.AlphaComplex(points = X.tolist())
+    simplex_tree = alpha_complex.create_simplex_tree(max_alpha_square = np.inf)
+    pers = simplex_tree.persistence()
+    gudhitime = time.time()-tic
+    Is2 = convertGUDHIPD(pers, len(Is1))   
+    
+    print("GUDHI Time: %.3g"%gudhitime)
+    
+    I1 = Is1[len(Is1)-1]
+    I2 = Is2[len(Is2)-1]
+    plt.scatter(I1[:, 0], I1[:, 1])
+    plt.scatter(I2[:, 0], I2[:, 1], 40, marker='x')
+    plt.show()
+    
 
 if __name__ == '__main__':
     testRips()
+    #testAlpha()
+    #compareAlpha()
--- README.md
+++ README.md
@@ -1,33 +1,38 @@
 # phatcech (Pronounced "Fat Check")
-The purpose of this code is to make it easy to run Cech Filtrations by wrapping around around Phat (The Persistent Homology Algorithms Toolbox).  Phat does most of the heavy lifting, but this code provides routines to construct and index simplices properly in a boundary matrix, as well as some plotting utilities.
+The purpose of this code is to make it easy to run custom, non-Rips filtrations (e.g. Cech and Alpha) by wrapping around around Phat (The Persistent Homology Algorithms Toolbox).  Phat does most of the heavy lifting, but this code provides routines to construct and index simplices properly in a boundary matrix, as well as some plotting utilities.
 
 ## Dependencies
 * Numpy/Matplotlib
 * PHAT (See below)
 
-## Compiling PHAT
-By default, the code calls a binary for phat from the command line, which you will need to compile.  This requires you to have CMake and OpenMP installed.  First, download and extract phat from this link: https://bitbucket.org/phat-code/phat.  Then, change into the phat directory and type
+## Testing
+You will need to install the Python wrapper for <a href = "https://github.com/xoltar/phat/">Phat</a> using pip
 
 ~~~~~ bash
-mkdir build
-cd build
-cmake ..
-make
+pip install phat
 ~~~~~
 
-This will generate a binary called "phat" in the build/ directory, which you will need to copy to the root of the PhatCech repository.  To test this, type
+Then you can test it
+
 ~~~~~ bash
 python PhatCech.py
 ~~~~~
 
-You should see the following image pop up:
+The output is something like
+```
+Constructing boundary matrix...
+Finished constructing boundary matrix (Elapsed Time 2.18)
+Computing persistence pairs...
+Finished computing persistence pairs (Elapsed Time 1.14)
+```
+And you should see the following image pop up:
 ![Example Rips Filtration](RipsExample.png "Rips on Noisy Circle")
 
 
-## Running Custom Cech Filtrations
-If you have a point cloud and a set of simplices with times at which they are added, you can call the function getCechDGMS() to compute the persistence diagrams associated to the Cech filtration you've defined (see doc string for more details).  For instance, assume we want to compute the Cech filtration where 4 vertices enter at time 0 and the edges and triangles are added in the pattern below (note how the triangles are not added the moment all of their edges are added, unlike Rips):
+## Running Custom Filtrations
+If you have a point cloud and a set of simplices with times at which they are added, you can call the function get_phat_dgms() to compute the persistence diagrams associated to the custom filtration you've defined (see doc string for more details).  For instance, assume we want to compute a filtration where 4 vertices enter at time 0 and the edges and triangles are added in the pattern below (note how the triangles are not added the moment all of their edges are added, unlike Rips):
 
-![Example Cech Filtration](CechExample.svg "Example of a Cech Filtration")
+![Example Cech Filtration](CustomExample.svg "Example of a Custom Filtration")
 
 Then we can execute the following code:
 
@@ -46,7 +51,7 @@ simplices = [([0], 0),
             ([2, 3], 3),
             ([0, 2, 3], 6)]
 #Compute persistence diagrams
-Is = getCechDGMs(simplices, returnInfs = True)
+Is = get_phat_dgms(simplices, returnInfs = True)
 print("H0:\n", Is[0])
 print("H1:\n", Is[1])
 ```
@@ -66,3 +71,39 @@ H1:
  [3 6]]
 ```
 Note that points that never die have a birth time which is "inf".  To exclude these, set returnInfs to false.
+
+## Alpha Filtrations
+Alpha filtrations are also supported.  This is the filtration that's obtained from the nerve of growing balls around points in Euclidean space which are the intersection of the Euclidean balls and the Voronoi regions around points.  In low ambient dimension, this filtration is much faster than the Rips filtration because there are far fewer simplices, though computing the Delaunay triangulation becomes more expensive in higher dimensions.
+
+An example alpha filtration is below of 400 points sampled on a 4-sphere in 5 dimensions
+```python
+import numpy as np
+import matplotlib.pyplot as plt
+from PhatCech import alpha_filtration
+from ripser import plot_dgms # (for plotting, must do "pip install ripser")
+
+X = np.random.randn(400, 5)
+X = X/np.sqrt(np.sum(X**2, 1)[:, None]) #Normalize points to the sphere
+dgms = alpha_filtration(X)
+plot_dgms([dgms[i] for i in range(len(dgms))])
+plt.title("Alpha Filtration")
+plt.show()
+```
+
+The output is 
+```
+Doing Delaunay triangulation...
+Finished Delaunay triangulation (Elapsed Time 0.873)
+Building alpha filtration...
+Finished building alpha filtration (Elapsed Time 88.9)
+Constructing boundary matrix...
+Finished constructing boundary matrix (Elapsed Time 2.12)
+Computing persistence pairs...
+Finished computing persistence pairs (Elapsed Time 0.214)
+```
+
+And the persistence diagrams are
+![Example Alpha Filtration](AlphaExample.svg "Example of an Alpha Filtration")
+
+Note that up to H4 on 400 points would be intractible with Rips, even with <a href = "https://github.com/Ripser/ripser">ripser</a>.  But it runs in a reasonable amount of time with Alpha.  The bottleneck with Alpha is constructing the filtration and computing many circumcenters.  Note that computing the persistence pairs takes even less time than H1 for Rips with only 100 points shown above.
+
