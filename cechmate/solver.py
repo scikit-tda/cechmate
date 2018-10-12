b4991702bd887b9d3fcf2c781f70a63b261b37f6
Nathaniel Saul <nat@saulgill.com>
2018-10-12 16:53:22 -0700

    organizing the solver

--- cechmate/solver.py
+++ cechmate/solver.py
@@ -4,33 +4,76 @@ import time
 import numpy as np
 import phat
 
-from .filtrations.simplex import Simplex
 
-
-def phat_diagrams(simplices, returnInfs=False, verbose=True):
+def phat_diagrams(simplices, hide_infs=True, verbose=True):
     """
     Do a custom filtration wrapping around phat
 
-    :param simplices: A list of lists of simplices and their distances\
-        the kth element is itself a list of tuples ([idx1, ..., idxk], dist)\
-        where [idx1, ..., idxk] is a list of vertices involved in the simplex\
+    Inputs
+    -------
+    simplices: A list of lists of simplices and their distances
+        the kth element is itself a list of tuples ([idx1, ..., idxk], dist)
+        where [idx1, ..., idxk] is a list of vertices involved in the simplex
         and "dist" is the distance at which the simplex is added
-    :param returnInfs: Whether or not to return points that never die
-    :param useWrapper: If true, call the phat binary as a subprocess.  If \
-        false, use Python bindings
-    :returns Is: A dictionary of persistence diagrams, where Is[k] is \
+    hide_infs: Whether or not to return points that never die
+
+    Returns
+    --------
+    dgms: A dictionary of persistence diagrams, where dgms[k] is 
         the persistence diagram for Hk 
     """
-    idxs2order = {}
-    ## Step 1: Go through simplices in ascending order of distance
+
+    ## Convert simplices representation to sparse pivot column
+    ordered_simplices = sorted(simplices, key=lambda x: x[1])
+    columns = simplices_to_sparse_pivot_column(ordered_simplices, verbose)
+   
+    ## Setup boundary matrix and reduce
+    if verbose:
+        print("Computing persistence pairs...")
+        tic = time.time()
+
+    boundary_matrix = phat.boundary_matrix(
+        columns=columns, representation=phat.representations.sparse_pivot_column
+    )
+    pairs = boundary_matrix.compute_persistence_pairs()
+    pairs.sort()
+
+    if verbose:
+        print(
+            "Finished computing persistence pairs (Elapsed Time %.3g)"
+            % (time.time() - tic)
+        )
+
+    ## Setup persistence diagrams by reading off distances
+    dgms = process_distances(pairs, ordered_simplices)
+
+    ## Add all unpaired simplices as infinite points
+    if not hide_infs:
+        dgms = add_unpaired(dgms, pairs, simplices)
+
+    ## Convert to arrays:
+    dgms = {i: np.array(dgm) for i, dgm in dgms.items()}
+
+    return dgms
+
+
+
+
+
+def simplices_to_sparse_pivot_column(ordered_simplices, verbose):
+    """
+
+    """
+
     idx = 0
     columns = []
-    ordsimplices = sorted([Simplex(s[0], s[1]) for s in simplices])
+    idxs2order = {}
+
     if verbose:
         print("Constructing boundary matrix...")
         tic = time.time()
-    for simplex in ordsimplices:
-        (idxs, dist) = (simplex.idxs, simplex.dist)
+
+    for idxs, dist in ordered_simplices:
         k = len(idxs)
         idxs = sorted(idxs)
         idxs2order[tuple(idxs)] = idx
@@ -52,59 +95,58 @@ def phat_diagrams(simplices, returnInfs=False, verbose=True):
                 collist.append(idxs2order[fidxs])
             collist = sorted(collist)
             columns.append((k - 1, collist))
+
         idx += 1
 
-    ## Step 2: Setup boundary matrix and reduce
     if verbose:
         print(
             "Finished constructing boundary matrix (Elapsed Time %.3g)"
             % (time.time() - tic)
         )
-        print("Computing persistence pairs...")
-        tic = time.time()
 
-    boundary_matrix = phat.boundary_matrix(
-        columns=columns, representation=phat.representations.sparse_pivot_column
-    )
-    pairs = boundary_matrix.compute_persistence_pairs()
-    pairs.sort()
+    return columns
 
-    if verbose:
-        print(
-            "Finished computing persistence pairs (Elapsed Time %.3g)"
-            % (time.time() - tic)
-        )
+def process_distances(pairs, ordered_simplices):
+    """ Setup persistence diagrams by reading off distances
+    """
+    
+    dgms = {}
+    posneg = np.zeros(len(ordered_simplices))
+    
+    for [bi, di] in pairs:
+        bidxs, bd = ordered_simplices[bi]
+        didxs, dd = ordered_simplices[di]
 
-    ## Step 3: Setup persistence diagrams by reading off distances
-    Is = {}  # Persistence diagrams
+        assert posneg[bi] == 0 and posneg[di] == 0
+        posneg[bi], posneg[di] = 1, -1
+
+        assert dd >= bd
+        assert len(bidxs) == len(didxs) - 1
+
+        p = len(bidxs) - 1
+        
+        # Don't add zero persistence pairs
+        if bd != dd:
+            dgms.setdefault(p, []).append([bd, dd])
+
+    return dgms
+
+
+def add_unpaired(dgms, pairs, simplices):
     posneg = np.zeros(len(simplices))
     for [bi, di] in pairs:
-        # Distances
-        (bidxs, bd) = [ordsimplices[bi].idxs, ordsimplices[bi].dist]
-        (didxs, dd) = [ordsimplices[di].idxs, ordsimplices[di].dist]
         assert posneg[bi] == 0
         assert posneg[di] == 0
         posneg[bi] = 1
         posneg[di] = -1
-        assert dd >= bd
-        assert len(bidxs) == len(didxs) - 1
-        p = len(bidxs) - 1
-        if not p in Is:
-            Is[p] = []
-        if bd == dd:
-            # Don't add zero persistence pairs
-            continue
-        Is[p].append([bd, dd])
 
-    ## Step 4: Add all unpaired simplices as infinite points
-    if returnInfs:
-        for i in range(len(posneg)):
-            if posneg[i] == 0:
-                (idxs, dist) = simplices[i]
-                p = len(idxs) - 1
-                if not p in Is:
-                    Is[p] = []
-                Is[p].append([dist, np.inf])
-    for i in range(len(Is)):
-        Is[i] = np.array(Is[i])
-    return Is
+    for i in range(len(posneg)):
+        if posneg[i] == 0:
+            (idxs, dist) = simplices[i]
+            p = len(idxs) - 1
+            if not p in dgms:
+                dgms[p] = []
+            dgms[p].append([dist, np.inf])
+
+    return dgms
+
