dbcb7425ac716f2b326b44cdcf9056a4c0c4fbcb
Nathaniel Saul <nat@saulgill.com>
2018-12-02 15:49:03 -0800

    add docstrings

--- cechmate/filtrations/cover.py
+++ cechmate/filtrations/cover.py
@@ -10,8 +10,28 @@ import itertools
 
 from .base import BaseFiltration
 
+"""
+    TODO: Build the Cech filtration that can be built using any metric.
+    
+    Can the Jaccard Filtration be built from this?
+
+"""
+
+class Cech(BaseFiltration):
+
+    def metric(self, elems):
+        pass
 
 class Cover(BaseFiltration):
+    def jaccard(self, covers):
+        """ Jaccard Distance 
+        """
+        covers_as_sets = list(map(set, covers))
+        intersection = set.intersection(*covers_as_sets)
+        union = set.union(*covers_as_sets)
+
+        return 1 - len(intersection) / len(union)
+    
     def build(self, covers):
 
         # Give each cover element a name.
@@ -22,7 +42,7 @@ class Cover(BaseFiltration):
 
         # TODO: be more intelligent about which combos we check
 
-        for k in range(2, self.max_dim + 1):
+        for k in range(2, self.max_dim + 2):
             for potentials in itertools.combinations(covers.keys(), k):
                 potential_sets = [covers[p] for p in potentials]
 
@@ -33,13 +53,5 @@ class Cover(BaseFiltration):
                     simplices.append((potentials, d))
 
         return simplices
-
-    def jaccard(self, covers):
-        covers_as_sets = list(map(set, covers))
-        intersection = set.intersection(*covers_as_sets)
-        union = set.union(*covers_as_sets)
-
-        return 1 - len(intersection) / len(union)
-
 
 __all__ = ["Cover"]
