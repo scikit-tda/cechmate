3962cf2296ed55769a8baee70906cbb8da64371b
Nathaniel Saul <nat@saulgill.com>
2019-02-13 18:36:46 -0800

    remove 2.7 support

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(name='cechmate',
           'sphinxcontrib-fulltoc'
         ]
       },
-      python_requires='>=2.7,!=3.1,!=3.2,!=3.3',
+      python_requires='>=3.4',
       classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
@@ -54,7 +54,6 @@ setup(name='cechmate',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Scientific/Engineering :: Mathematics',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
